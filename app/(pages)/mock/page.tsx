135dcce54cd93e96d04adf578e3a50ca9c30d082
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-09-08 00:32:26 +0900

    feat: firebase에 mock data 추가 함수 완성

---
+++ app/(pages)/mock/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import AdBannerButton from '@/mock/AdBannerButton'
+import CardAddButton from '@/mock/CardAddButton'
+
+export default function Mock() {
+  return (
+    <main>
+      <CardAddButton />
+      <AdBannerButton />
+    </main>
+  )
+}
---
+++ app/constants/Collection.ts
@@ -0,0 +1,5 @@
+export const COLLECTIONS = {
+  CARD: 'CARD',
+  ADBANNER: 'ADDBANNER',
+  USER: 'user',
+} as const
